04b1778ce14ac70b80b9e7176f02e0a191427340
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-14 15:31:27 +0300

    Initial Commit

---
+++ ToDoSwiftData/ToDoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftDataApp.swift
+//  ToDoSwiftData
+//
+//  Created by onur on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
